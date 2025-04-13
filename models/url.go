ecc883da7a635171339e3882bf06a6909d617e81
iqbalpa <iqbalpahlevi07@gmail.com>
2025-04-13 10:47:27 +0700

    feat: add desc in model

--- models/url.go
+++ models/url.go
@@ -9,8 +9,15 @@ import (
 type ShortenedUrl struct {
 	ID uuid.UUID `gorm:"primaryKey;type:uuid;default:gen_random_uuid()"`
 
+	// LongUrl is the original URL that needs to be shortened.
+	// It should be a valid URL format.
+	// for example: https://www.example.com/some/long/url
 	LongUrl string `gorm:"not null"`
 
+	// ShortUrl is the shortened version of the LongUrl.
+	// It omits the protocol (http/https) and domain name.
+	// It should be unique and not null.
+	// for example: /abc123
 	ShortUrl string `gorm:"unique;not null"`
 
 	CreatedAt time.Time `gorm:"autoCreateTime"`
